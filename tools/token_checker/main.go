1b279e3efc0cd6f3160e3854815ff5490e6a4dd0
178inaba <178inaba@users.noreply.github.com>
2019-02-15 01:59:06 +0900

    Add token_checker

---
+++ tools/token_checker/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("admin.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	idToken := "token"
+	token, err := client.VerifyIDToken(ctx, idToken)
+	if err != nil {
+		log.Fatalf("error verifying ID token: %v\n", err)
+	}
+
+	log.Printf("Verified ID token: %#v\n", token)
+}
